28438a51d17e5356c1bd166ae45205eeb433a401
Jonathan-R-Anderson <77250765+Jonathan-R-Anderson@users.noreply.github.com>
2025-11-17 10:54:50 -0500

    Include hardware probe module in kernel build

--- buildscript.sh
+++ buildscript.sh
@@ -163,6 +163,7 @@ KERNEL_SOURCES=(
   "src/minimal_os/kernel/exceptions.d"
   "src/minimal_os/console.d"
   "src/minimal_os/serial.d"
+  "src/minimal_os/hardware.d"
   "src/minimal_os/compiler.d"
   "src/minimal_os/posix.d"
   "src/minimal_os/posixutils/registry.d"

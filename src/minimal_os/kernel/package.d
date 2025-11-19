694998f3efbf44049563bdeb0f52d776d09989f8
Jonathan-R-Anderson <77250765+Jonathan-R-Anderson@users.noreply.github.com>
2025-11-19 08:56:53 -0500

    Add process memory layout abstractions

--- src/minimal_os/kernel/package.d
+++ src/minimal_os/kernel/package.d
@@ -2,6 +2,7 @@ module minimal_os.kernel;
 
 public import minimal_os.kernel.kernel;
 public import minimal_os.kernel.memory;
+public import minimal_os.kernel.process_memory;
 public import minimal_os.kernel.posixbundle;
 public import minimal_os.kernel.shell_integration;
 public import minimal_os.kernel.exceptions;
---
+++ tests/process_memory_test_runner.d
@@ -0,0 +1,5 @@
+module tests.process_memory_test_runner;
+
+import minimal_os.kernel.process_memory;
+
+void main() {}

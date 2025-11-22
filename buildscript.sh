cf04ba6f749b954020e3b894b50b761131a905e4
Jonathan-R-Anderson <77250765+Jonathan-R-Anderson@users.noreply.github.com>
2025-11-22 13:14:25 -0500

    Include font stack in kernel build

--- buildscript.sh
+++ buildscript.sh
@@ -198,6 +198,7 @@ KERNEL_SOURCES=(
   "src/minimal_os/serial.d"
   "src/minimal_os/hardware.d"
   "src/minimal_os/display/canvas.d"
+  "src/minimal_os/display/font_stack.d"
   "src/minimal_os/display/framebuffer.d"
   "src/minimal_os/display/input_pipeline.d"
   "src/minimal_os/display/input_handler.d"

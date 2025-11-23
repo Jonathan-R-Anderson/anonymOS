30d12717c08494c6ceb541c55711799591464203
Jonathan Anderson <jranderson404@gmail.com>
2025-11-23 08:48:55 -0500

    debugging

---
+++ src/minimal_os/kernel/compiler_builder_entry.d
@@ -0,0 +1,7 @@
+module minimal_os.kernel.compiler_builder_entry;
+
+pragma(mangle, "compilerBuilderProcessEntry")
+export extern(C) @nogc nothrow void compilerBuilderProcessEntry(const(char*)* argv, const(char*)* envp)
+{
+    // Minimal stub to satisfy linker; no operation.
+}

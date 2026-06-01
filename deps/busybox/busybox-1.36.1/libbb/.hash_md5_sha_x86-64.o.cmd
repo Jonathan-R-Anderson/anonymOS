75b9f9a3179cf7ff795be5ef4417558520c88557
Jonathan Anderson <jonathan.r.anderson@protonmail.com>
2026-06-01 15:49:30 -0500

    Batch commit 64

---
+++ deps/busybox/busybox-1.36.1/libbb/.hash_md5_sha_x86-64.o.cmd
@@ -0,0 +1,9 @@
+cmd_libbb/hash_md5_sha_x86-64.o := /home/bruns/Documents/EpinAnonymOS/deps/musl/install/bin/musl-clang -Wp,-MD,libbb/.hash_md5_sha_x86-64.o.d  -std=gnu99 -Iinclude -Ilibbb  -include include/autoconf.h -D_GNU_SOURCE -DNDEBUG  -DBB_VER='"1.36.1"' -Wall -Wshadow -Wwrite-strings -Wundef -Wstrict-prototypes -Wunused -Wunused-parameter -Wunused-function -Wunused-value -Wmissing-prototypes -Wmissing-declarations -Wno-format-security -Wdeclaration-after-statement -Wold-style-definition -finline-limit=0 -fno-builtin-strlen -fomit-frame-pointer -ffunction-sections -fdata-sections -funsigned-char -falign-functions=1 -falign-jumps=1 -falign-labels=1 -falign-loops=1 -fno-unwind-tables -fno-asynchronous-unwind-tables -fno-builtin-printf -Oz -O2 -pipe       -c -o libbb/hash_md5_sha_x86-64.o libbb/hash_md5_sha_x86-64.S
+
+deps_libbb/hash_md5_sha_x86-64.o := \
+  libbb/hash_md5_sha_x86-64.S \
+    $(wildcard include/config/sha1/small.h) \
+
+libbb/hash_md5_sha_x86-64.o: $(deps_libbb/hash_md5_sha_x86-64.o)
+
+$(deps_libbb/hash_md5_sha_x86-64.o):

3c66ae4b5564bbde17ed04ceb971dc2a08d09c04
Jonathan Anderson <jonathan.r.anderson@protonmail.com>
2026-06-01 15:48:29 -0500

    Batch commit 26

---
+++ deps/busybox/busybox-1.36.1/applets/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_applets/built-in.o :=  /home/bruns/Documents/EpinAnonymOS/deps/musl/install/bin/musl-clang -nostdlib -nostdlib --static -r -o applets/built-in.o applets/applets.o
---
+++ deps/busybox/busybox-1.36.1/archival/.lib.a.cmd
@@ -0,0 +1,1 @@
+cmd_archival/lib.a := rm -f archival/lib.a; ar  rcs archival/lib.a archival/bbunzip.o archival/bzip2.o archival/gzip.o archival/tar.o

41f52ce63859317f6e30ae20ce15df7660222c87
Jonathan Anderson <jonathan.r.anderson@protonmail.com>
2026-06-02 23:17:42 -0500

    Batch commit 26

---
+++ deps/elogind/elogind-255.4/src/basic/audit-util.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <sys/types.h>
+
+#define AUDIT_SESSION_INVALID UINT32_MAX
+
+int audit_session_from_pid(pid_t pid, uint32_t *id);
+int audit_loginuid_from_pid(pid_t pid, uid_t *uid);
+
+#if 0 /// UNNEEDED by elogind
+bool use_audit(void);
+#endif // 0
+
+static inline bool audit_session_is_valid(uint32_t id) {
+        return id > 0 && id != AUDIT_SESSION_INVALID;
+}
---
+++ deps/elogind/elogind-255.4/src/basic/build.h
@@ -0,0 +1,6 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+extern const char* const elogind_features;
+
+int version(void);
